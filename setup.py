a99c60d39c38e44106f6c4979e16f3bf6b6d723b
Gaurav Singh <gauravkumaya@gmail.com>
2025-09-11 01:44:11 +0530

    File structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DocuMate",
+    version="0.0.0",
+    author="Gaurav Singh",
+    author_email="gauravkumaya@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
